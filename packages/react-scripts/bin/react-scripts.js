0477008d670135eb6ec60f65826bd17eb506f220
Carl Vitullo <carl.vitullo@gmail.com>
2017-10-17 16:16:48 -0400

    Use JS scripts for test and eject

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -18,10 +18,36 @@ const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 switch (script) {
-  case 'build':
-  case 'eject':
+  case 'test':
+  case 'eject': {
+    const result = spawn.sync(
+      'node',
+      nodeArgs
+        .concat(require.resolve('../scripts/' + script))
+        .concat(args.slice(scriptIndex + 1)),
+      { stdio: 'inherit' }
+    );
+    if (result.signal) {
+      if (result.signal === 'SIGKILL') {
+        console.log(
+          'The build failed because the process exited too early. ' +
+            'This probably means the system ran out of memory or someone called ' +
+            '`kill -9` on the process.'
+        );
+      } else if (result.signal === 'SIGTERM') {
+        console.log(
+          'The build failed because the process exited too early. ' +
+            'Someone might have called `kill` or `killall`, or the system could ' +
+            'be shutting down.'
+        );
+      }
+      process.exit(1);
+    }
+    process.exit(result.status);
+    break;
+  }
   case 'start':
-  case 'test': {
+  case 'build': {
     const result = spawn.sync(
       require.resolve('../scripts/' + script + '.sh'),
       nodeArgs.concat(args.slice(scriptIndex + 1)),
